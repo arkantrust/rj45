9122eb13ae196026c28bd70652ffa1f043b1c00c
Daniel Mejia <danielmax547@gmail.com>
2024-10-20 00:09:34 -0500

    New window per-Event button Postlog

--- web/js/postlog.js
+++ web/js/postlog.js
@@ -1,20 +1,34 @@
-function openNextwindow() {
-    const windowsContainer = document.querySelector('.window');
-    windowsContainer.style.transform = 'translateX(-50%)';
-}
+
+let visible = false;
 
 function openNextwindow() {
     const windowsContainer = document.querySelector('.next-window'); // Seleccionamos la ventana siguiente
-    const windowsContainerMain = document.querySelector('.window'); // Seleccionamos el contenedor principal
+    const windowsContainerMain = document.querySelector('.current-window'); // Seleccionamos el contenedor principal
 
     // Cambiar a la siguiente ventana
-    windowsContainerMain.style.transform = 'translateX(-50%)'; // Mueve el contenedor principal a la izquierda
-    // Cambia la visibilidad de .next-window
-    // windowsContainer.classList.add('visible'); // Añade la clase para hacerla visible aun en beta
+    windowsContainerMain.style.transform = 'translateX(0%)'; // Mueve el contenedor principal a la izquierda
+    windowsContainer.classList.add('visible'); // Añade la clase para hacerla visible aun en beta
+
+    toggleWindow()
+
+
 }
 
-// Si quieres que al cargar la página no se muestre la siguiente ventana, asegúrate de que el HTML esté configurado correctamente
-document.addEventListener('DOMContentLoaded', function() {
-    const nextWindow = document.querySelector('.next-window');
-    nextWindow.classList.remove('visible'); // Asegúrate de que esté inicialmente oculta
-});
+
+function toggleWindow() {
+    {
+        const nextWindow = document.querySelector('.next-window');
+        const currentWindow = document.querySelector('.current-window');
+
+        if (visible) {
+            nextWindow.classList.remove('visible');
+            currentWindow.style.transform = 'translateX(50%)'; // Retorna a la posición inicial
+        } else {
+            
+            nextWindow.classList.add('visible');
+            currentWindow.style.transform = 'translateX(0%)'; // Mueve el contenedor principal
+        }
+
+        visible = !visible; // Alterna el estado de visibilidad
+    }
+}
