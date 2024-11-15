1ad976092df9955c0dedee3bfdb5b7ddc1352dbe
David Dulce <davidulce26@gmail.com>
2024-11-14 21:30:02 -0500

    feat: aws infra

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+// TODO: set environment variables from app.terraform.io
+
+// TODO: run setup script
