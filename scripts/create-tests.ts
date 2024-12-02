0afca762294a6e34bf2b119714d5c39a30035a64
David Dulce <davidulce26@gmail.com>
2024-12-01 19:22:00 -0500

    feat: test mock script

--- scripts/create-tests.ts
+++ scripts/create-tests.ts
@@ -9,18 +9,25 @@ interface Vector {
 interface Measurement {
     accel: Vector,
     gyro: Vector,
-    timestamp: number
+    time: number
 }
 
 interface Test {
+    id: string,
     type: 'footing' | 'heeling',
-    measurements: Measurement[]
+    measurements: Measurement[],
+    timestamp: number,
+    evaluatorId: number,
+    patientId: number
 }
+
+const NUM_TESTS = 1;
 
 // Create tests with random data
 const tests: Test[] = [];
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < NUM_TESTS; i++) {
     const measurements: Measurement[] = [];
+    const t0 = Date.now()
     for (let j = 0; j < 210; j++) {
         measurements.push({
             accel: {
@@ -33,14 +40,18 @@ for (let i = 0; i < 7; i++) {
                 y: Math.random(),
                 z: Math.random()
             },
-            timestamp: Date.now()
+            time: Date.now() - t0
         });
+        await Bun.sleep(24);
     }
     tests.push({
+        id: Bun.randomUUIDv7(),
         type: Math.random() > 0.5 ? 'footing' : 'heeling',
-        measurements
+        measurements,
+        timestamp: Date.now(),
+        evaluatorId: Math.floor(Math.random() * 10),
+        patientId: Math.floor(Math.random() * 10)
     });
-    await Bun.sleep(24);
 }
 
 // Write a json file with the tests
@@ -54,4 +65,4 @@ if (!exists) {
 }
 
 // Log the result
-console.log(`Wrote tests to ${path}`);
+console.log(`Wrote ${NUM_TESTS} test(s) to ${path} with ${tests[0].measurements.length} measurements.`);
