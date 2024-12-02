d6d4b5d6b6e1edce4c341fd02670d24bba7dab5a
David Dulce <davidulce26@gmail.com>
2024-12-01 19:21:00 -0500

    fix: comments form

--- web/src/services/test.js
+++ web/src/services/test.js
@@ -25,3 +25,34 @@ export async function getTests() {
         console.error(error);
     }
 }
+
+export async function addComment(id, comment) {
+    try {
+        const res = await fetchApi(endpoint + `/${id}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ comment })
+            });
+        if (!res.ok)
+            throw new Error('Error al obtener el test');
+        
+        return await res.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getComments(testId) {
+    try {
+        const res = await fetchApi(endpoint + `/${testId}/comments`);
+        if (!res.ok)
+            throw new Error('Error al obtener los tests');
+        
+        return await res.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
